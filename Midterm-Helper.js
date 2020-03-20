bf7d7de6e2ae18209b0d3c30fde5aa22fa710a00
Jacob Santoni <jlsantoni2@gmail.com>
2020-03-20 12:43:42 -0400

    added it again

---
+++ Midterm-Helper.js
@@ -0,0 +1,11 @@
+
+
+function openSubmissionRange(startRow, endRow){
+    var table = document.getElementById("grades");
+    var rows = table.children[1]
+    for (var i = endRow-1; i>startRow-1; i--){
+        var link = rows.children[i].children[1].children[2].href
+        window.open(link, '_blank');
+    }
+
+}
